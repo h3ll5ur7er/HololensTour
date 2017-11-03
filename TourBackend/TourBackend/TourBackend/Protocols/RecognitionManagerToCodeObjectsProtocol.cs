5fa3f2338b14c049f3f8bc6eff36845a05364350
kishtrim <markus.kiser@hotmail.com>
2017-11-03 17:16:35 +0100

    Protokolländerungen und KonstruktorenTesting der Message-Klassen
    Alle unnötigen Protokolle sind gelöscht worden und die zwei Protokolle ControlToRecognitionManager und RecognitionManagerToCodeObjects sind mal fertig gestellt. Dabei hat jede Message -Klasse zwei Konstruktoren,
    einen mit zwei Argumenten (_senderPID, _messageID) und einer mit drei Argumenten (_senderID, _messageID, _targetActor). Die Tests sind für alle Konstruktoren der oben beschriebenen Klassen geschrieben und grün.

---
+++ TourBackend/TourBackend/TourBackend/Protocols/RecognitionManagerToCodeObjectsProtocol.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Proto;
+
+namespace TourBackend
+{ 
+    /* 
+     * This is kind of a pseude Protocoll, cause we have all the same messages from the control to the 
+     * recognition manager as from the recognition manager to the CodeObjects. Therefore to know which
+     * messages are allowed between the recognition manager and the code objects, you should go to the
+     * ControlToRecognitionManagerProtocol.cs file and read more there...
+    */
+}
