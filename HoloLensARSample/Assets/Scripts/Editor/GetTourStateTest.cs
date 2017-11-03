e99b36ffb0bb2a538bd3bb0b1beb3f35829b40db
Moritz Scherer <scheremo@student.ethz.ch>
2017-11-03 13:20:03 +0100

    New test and implementation
    Test adds new entry into SyncObject and checks if after one update the copy of GetTourState dictionary is equal to it. Now also implemented. (+6 squashed commit)
    
    Squashed commit:
    
    [50dc291] New Test and Implementation
    
    Added a test to check if time stamp will be updated. After test was failing added comparison of timestamp and change of timestamp in the GetTourState script.
    
    [546eaba] First test in Unity
    
    Inplemented a test to see if the GetTourState starts in the right way and creates a SyncObject with the expected data
    
    [f64c2d5] Temporary changes
    
    [1509b65] Temporary changes
    
    [e83877e] gitignore change, SyncObject erstellen
    
    SyncObject mit Konstruktor und allen ?bergabeparametern. Dictonary Erstellung
    
    [9a30493] .gitignore ?nderungen
    
    Assets hinzugef?gt

---
+++ HoloLensARSample/Assets/Scripts/Editor/GetTourStateTest.cs
@@ -0,0 +1,108 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace TourBackend
+{
+    public class GetTourStateTest
+    {
+
+        [Test]
+        public void GetTourState_initialize_empty_SyncObject_correctly()
+        {
+            // Use the Assert class to test conditions.
+            //Testobject
+            //GameObject test = new GameObject();
+            //Create getTourState object
+
+            GetTourState getTourState = new GameObject().AddComponent<GetTourState>();
+            getTourState.Start();
+
+            //Compare if the object is as expected empty
+            Assert.AreEqual(getTourState.syncObject.objectid, "syncid");
+            CollectionAssert.AreEqual(getTourState.syncObject.dict, new Dictionary<string, CodeObject>());
+            Assert.AreEqual(getTourState.syncObject.timestamp, 10);
+
+        }
+
+        [Test]
+        public void GetTourState_lasttimestamp_gets_updated_to_new_timestamp_of_SyncObject()
+        {
+            //Initiate GetTourState
+            GetTourState getTourState = new GameObject().AddComponent<GetTourState>();
+
+            //Create SyncObject
+            getTourState.Start();
+
+            //initial timestamp
+            System.Int64 inititialTimestamp = 10;
+
+            //Check old time Stamp
+            Assert.AreEqual(getTourState.syncObject.timestamp, inititialTimestamp);
+
+            //Update GetTourState
+            getTourState.Update();
+            //Check if copy has right timestamp
+            Assert.AreEqual(getTourState.lasttimestamp, inititialTimestamp);
+
+            //update timestamp
+            System.Int64 newTimestamp = 12;
+
+            //Change timestamp
+            getTourState.syncObject.timestamp = newTimestamp;
+
+            //Update GetTourState
+            getTourState.Update();
+
+            //Check if timestamp is changed on syncObject
+            Assert.AreEqual(getTourState.lasttimestamp, newTimestamp);
+
+        }
+
+        [Test]
+        public void GetTourState_copy_new_dict_entry_of_SyncObject()
+        {
+            //Initiate GetTourState
+            GetTourState getTourState = new GameObject().AddComponent<GetTourState>();
+
+            //Create SyncObject
+            getTourState.Start();
+
+            //Check if CopyOfDict is a new Dictionary
+            CollectionAssert.AreEqual(getTourState.CopyOfDict, new Dictionary<string, CodeObject>());
+
+            //Adding a new Dictonary Object and change timestamp
+            getTourState.syncObject.dict.Add("objectid", new CodeObject("objectid", 1,new int[] { 0, 0, 0 },new int[] { 0, 0, 0 }));
+            getTourState.syncObject.timestamp = 12;
+
+            //Update GetTourState
+            getTourState.Update();
+
+            //Check if CopyOfDict is updated
+            Assert.AreEqual(getTourState.CopyOfDict.ContainsKey("objectid"),true);
+            //Check if it's the right CodeObject
+            Assert.AreEqual(getTourState.CopyOfDict["objectid"].objectid, "objectid");
+            Assert.AreEqual(getTourState.CopyOfDict["objectid"].mediaid, 1);
+            Assert.AreEqual(getTourState.CopyOfDict["objectid"].position, new int[] { 0, 0, 0 });
+            Assert.AreEqual(getTourState.CopyOfDict["objectid"].rotation, new int[] { 0, 0, 0 });
+
+        }
+
+        // A UnityTest behaves like a coroutine in PlayMode
+        // and allows you to yield null to skip a frame in EditMode
+        [UnityTest]
+        public IEnumerator GetTourStateTestWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // yield to skip a frame
+            yield return null;
+
+
+
+        }
+    }
+
+}
--- HoloLensARSample/Assets/Scripts/GetTourState.cs
+++ HoloLensARSample/Assets/Scripts/GetTourState.cs
@@ -2,17 +2,64 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GetTourState : MonoBehaviour {
+namespace TourBackend
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    public class GetTourState : MonoBehaviour
+    {
+        public SyncObject syncObject;
+        public Dictionary<string, CodeObject> CopyOfDict = new Dictionary<string, CodeObject>();
+        public System.Int64 lasttimestamp;
+        // Use this for initialization
+        public void Start()
+        {
+            //Initialisierung SyncObject
+            lasttimestamp = 10;
+            syncObject = new SyncObject("syncid", new Dictionary<string, CodeObject>(), lasttimestamp);
+            CopyOfDict = new Dictionary<string, CodeObject>();
 
-        
-		
-	}
+        }
+
+        // Update is called once per frame
+        public void Update()
+        {
+            //GetCopyOfDictionary
+            //ToDo: Test if lock is free
+
+
+            //Only copy if timestamp changed
+            if (syncObject.timestamp != lasttimestamp)
+            {
+                lasttimestamp = syncObject.timestamp;
+                CopyOfDict = syncObject.dict;
+                //ReadDictionaryData
+                foreach (string objectid in CopyOfDict.Keys)
+                {
+                    //CodeObject with current key
+                    CodeObject obj = CopyOfDict[objectid];
+                    //TestTypeOfData
+                    switch (obj.mediaid)
+                    {
+                        case 1:
+                            //instantiate Texture, set position and rotation
+                            break;
+                        case 2:
+                            //instantiate 3D model, set position and rotation
+                            break;
+                        case 3:
+                            //instantiate video, set position and rotation
+                            break;
+                        default:
+                            //media without mediaid -> error
+                            break;
+
+
+                    }
+
+                }
+            }
+
+
+        }
+    }
 }
