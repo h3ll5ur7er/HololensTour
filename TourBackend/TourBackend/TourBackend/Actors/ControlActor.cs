12889872e26c020994a6ddc6e48ddc842ec01b8b
kishtrim <markus.kiser@hotmail.com>
2017-11-06 15:31:11 +0100

    Änderungen an Actors: Erstellung eines neuen SyncActor
    SyncActor wird von ControlActor erstellt wie wir es mit Martin besprochen haben...

--- TourBackend/TourBackend/TourBackend/Actors/ControlActor.cs
+++ TourBackend/TourBackend/TourBackend/Actors/ControlActor.cs
@@ -9,13 +9,21 @@ namespace TourBackend
 {
     public class ControlActor : IActor
     {
-
         protected string Id { get; }
 
         public ControlActor(string id)
         {
             Id = id;
+            // create a new actor recognitionManager if an ControlActor gets created
+            var propsRecognitionManager = Actor.FromProducer(
+                        () => new RecognitionManager("RecognitionManager"));
+            var pidRecognitionManager = Actor.Spawn(propsRecognitionManager);
+            // create a new actor if an ControlActor gets created
+
+
         }
+
+
 
         public Task ReceiveAsync(IContext context)
         {
