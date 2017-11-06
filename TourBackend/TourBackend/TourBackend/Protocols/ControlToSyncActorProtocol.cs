95002f36c4dcb412ed5f3d09a118812c7e63bc6f
Moritz Scherer <scheremo@student.ethz.ch>
2017-11-06 17:26:46 +0100

    TourBackend Änderungen
    Erstellen des SyncObjectActors & zugehöriges Protokoll

--- TourBackend/TourBackend/TourBackend/Protocols/ControlToSyncActorProtocol.cs
+++ TourBackend/TourBackend/TourBackend/Protocols/ControlToSyncActorProtocol.cs
@@ -7,17 +7,40 @@ using System.Threading.Tasks;
 namespace TourBackend
 {
 
+    public class SyncObject
+    {
+
+        private Object thisLock = new Object();
+
+        public string objectid;
+        public Dictionary<string, CodeObject> dict;
+
+        //Basic Konstruktor 
+        public SyncObject(string _objectid, Dictionary<string, CodeObject> _dict)
+        {
+            objectid = _objectid;
+            dict = _dict;
+        }
+
+    }
+
     public class CodeObject {
+
+        public string objectid;
+        public int mediaid;
         public int[] position;
         public int[] rotation;
 
-        public CodeObject(int[] _position, int[] _rotation) {
+        public CodeObject(string _objectid, int _mediaid, int[] _position, int[] _rotation) {
+            objectid = _objectid;
+            mediaid = _mediaid;
             position = _position;
             rotation = _rotation;
         }
     }
 
-    public class WriteCurrentTourState {
+    public class WriteCurrentTourState
+    {
 
         public string id;
         public Dictionary<string, CodeObject> dict;
