2abe56eda1752c063f60346b8ddc3b702c22942d
Moritz Scherer <scheremo@student.ethz.ch>
2017-11-06 17:02:39 +0100

    Tourbackend Änderungen
    Squashable; SyncActor geändert

--- TourBackend/TourBackend/TourBackend/Protocols/ControlToSyncActorProtocol.cs
+++ TourBackend/TourBackend/TourBackend/Protocols/ControlToSyncActorProtocol.cs
@@ -4,9 +4,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TourBackend.Protocols
+namespace TourBackend
 {
-    class ControlToSyncActorProtocol
-    {
+
+    public class CodeObject {
+        public int[] position;
+        public int[] rotation;
+
+        public CodeObject(int[] _position, int[] _rotation) {
+            position = _position;
+            rotation = _rotation;
+        }
     }
+
+    public class WriteCurrentTourState {
+
+        public string id;
+        public Dictionary<string, CodeObject> dict;
+
+        public WriteCurrentTourState(string _id, Dictionary<string, CodeObject> _dict)
+        {
+            id = _id;
+            dict = _dict;
+        }
+    }
+
+    public class RespondWriteCurrentTourState {
+
+        public string id;
+
+        public RespondWriteCurrentTourState(string _id) {
+            id = _id;
+        }
+
+    }
+
 }
