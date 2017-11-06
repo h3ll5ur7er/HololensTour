abe630a0d664ac89b3adb838d13a0ae1060e8c01
Selim <snaji@student.ethz.ch>
2017-11-06 17:01:38 +0100

    gitignore change, SyncObject erstellen
    SyncObject mit Konstruktor und allen Übergabeparametern. Dictonary Erstellung

---
+++ HoloLensARSample/Assets/Scripts/SyncObject.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TourBackend
+{
+
+
+    public class FullObject
+    {
+        /*mediaid
+        1 = texture
+        2 = 3D mesh
+        3 = video
+       ...erweiterbar
+       */
+        public string objectid;
+        public int mediaid;
+        public int[] position;
+        public int[] rotation;
+
+        //Basic Konstruktor 
+        public FullObject(string _objectid, int _mediaid, int[] _position, int[] _rotation, string _type)
+        {
+            objectid = _objectid;
+            mediaid = _mediaid;
+            position = _position;
+            rotation = _rotation;
+        }
+
+    }
+
+
+    //Dictionary with ID and Object with Mesh, position, rotation, type
+    public Dictionary<int, FullObject> dict = new Dictionary<int, FullObject>();
+
+
+
+
+
+}
