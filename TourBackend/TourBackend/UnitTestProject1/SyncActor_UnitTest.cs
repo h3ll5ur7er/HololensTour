c6b7b0b3257cfa62b876fb60ce6611288e2664f6
Moritz Scherer <scheremo@student.ethz.ch>
2017-11-07 09:13:16 +0100

    TourBackend Änderungen
    Neues Feature eingefügt & getestet:
    TimeStamp auf dem SyncObject, Stopwatch auf dem SyncActor

--- TourBackend/TourBackend/UnitTestProject1/SyncActor_UnitTest.cs
+++ TourBackend/TourBackend/UnitTestProject1/SyncActor_UnitTest.cs
@@ -5,6 +5,7 @@ using FluentAssertions;
 using Proto;
 using System.Threading.Tasks;
 using System.Diagnostics;
+using System.Threading;
 
 namespace TourBackend
 {
@@ -32,36 +33,73 @@ namespace TourBackend
             var propsSyncActor1 = Actor.FromProducer(() => new SyncActor("SyncActor1", obj));
             var pidSyncActor1 = Actor.Spawn(propsSyncActor1);
 
-            var reply = await pidSyncActor1.RequestAsync<RespondWriteCurrentTourState>(new WriteCurrentTourState("dab",dict), TimeSpan.FromSeconds(1));
+            var reply = await pidSyncActor1.RequestAsync<RespondWriteCurrentTourState>(new WriteCurrentTourState("dab", dict), TimeSpan.FromSeconds(1));
 
-            CollectionAssert.AreEqual((System.Collections.ICollection) obj.dict, (System.Collections.ICollection) dict);
+            CollectionAssert.AreEqual((System.Collections.ICollection)obj.dict, (System.Collections.ICollection)dict);
+
+        }
+
+        [TestMethod]
+        public async Task SyncActor_needs_to_write_different_timestamps_for_different_writes()
+        {
+            var dict = new Dictionary<string, CodeObject>();
+            var cd1 = new CodeObject("cd1", 1, new[] { 1, 2, 3 }, new[] { 4, 5, 6 });
+            var cd2 = new CodeObject("cd2", 1, new[] { 4, 7, 8 }, new[] { 1, 19, 3 }); // Just build two "random" CodeObjects
+            var cd3 = new CodeObject("cd3", 2, new[] { 57, 34, 124 }, new[] { 1235,35, 757 }); // Just build to "random" CodeObjects
+
+            dict.Add("cd1", cd1);
+            dict.Add("cd2", cd2);
+
+            Assert.AreEqual(dict["cd1"], cd1); // Check that the dict contains the right CodeObjects
+            Assert.AreEqual(dict["cd2"], cd2);
+
+            var obj = new SyncObject("sync1", new Dictionary<string, CodeObject>());
+
+            CollectionAssert.AreEqual((System.Collections.ICollection)obj.dict, (System.Collections.ICollection)new Dictionary<string, CodeObject>()); // Check that SyncObject has no dict
+
+            var propsSyncActor1 = Actor.FromProducer(() => new SyncActor("SyncActor1", obj));
+            var pidSyncActor1 = Actor.Spawn(propsSyncActor1);
+
+            var reply = await pidSyncActor1.RequestAsync<RespondWriteCurrentTourState>(new WriteCurrentTourState("dab", dict), TimeSpan.FromSeconds(1));
+
+            var timestamp1 = obj.timestamp;
+
+            Thread.Sleep(1); // Actors might be faster than 1 ms, but 1 ms is maximal precision of stopwatch
+
+            dict.Add("cd3", cd3);
+            var reply2 = await pidSyncActor1.RequestAsync<RespondWriteCurrentTourState>(new WriteCurrentTourState("dab2", dict), TimeSpan.FromSeconds(1));
+
+            var timestamp2 = obj.timestamp;
+
+            Assert.AreNotEqual(timestamp1,timestamp2);
+
+        }
+        [TestMethod]
+        public async Task SyncActor_needs_to_respond_after_writing_on_SyncObject()
+        {
+
+            var dict = new Dictionary<string, CodeObject>();
+            var cd1 = new CodeObject("cd1", 1, new[] { 1, 2, 3 }, new[] { 4, 5, 6 });
+            var cd2 = new CodeObject("cd2", 1, new[] { 4, 7, 8 }, new[] { 1, 19, 3 }); // Just build to "random" CodeObjects
+
+            dict.Add("cd1", cd1);
+            dict.Add("cd2", cd2);
+
+            Assert.AreEqual(dict["cd1"], cd1); // Check that the dict contains the right CodeObjects
+            Assert.AreEqual(dict["cd2"], cd2);
+
+            var obj = new SyncObject("sync1", null);
+
+            CollectionAssert.AreEqual(obj.dict, null); // Check that SyncObject has no dict
+
+            var propsSyncActor1 = Actor.FromProducer(() => new SyncActor("SyncActor1", obj));
+            var pidSyncActor1 = Actor.Spawn(propsSyncActor1);
+
+            var reply = await pidSyncActor1.RequestAsync<RespondWriteCurrentTourState>(new WriteCurrentTourState("req1", dict), TimeSpan.FromSeconds(1)); // Expect Answer
+
+            Assert.AreEqual(reply.id, "req1");
+
+        }
 
     }
-    public async void SyncActor_needs_to_respond_after_writing_on_SyncObject()
-    {
-
-        var dict = new Dictionary<string, CodeObject>();
-        var cd1 = new CodeObject("cd1", 1, new[] { 1, 2, 3 }, new[] { 4, 5, 6 });
-        var cd2 = new CodeObject("cd2", 1, new[] { 4, 7, 8 }, new[] { 1, 19, 3 }); // Just build to "random" CodeObjects
-
-        dict.Add("cd1", cd1);
-        dict.Add("cd2", cd2);
-
-        Assert.AreEqual(dict["cd1"], cd1); // Check that the dict contains the right CodeObjects
-        Assert.AreEqual(dict["cd2"], cd2);
-
-        var obj = new SyncObject("sync1", null);
-
-        CollectionAssert.AreEqual(obj.dict, null); // Check that SyncObject has no dict
-
-        var propsSyncActor1 = Actor.FromProducer(() => new SyncActor("SyncActor1", obj));
-        var pidSyncActor1 = Actor.Spawn(propsSyncActor1);
-
-        var reply = await pidSyncActor1.RequestAsync<RespondWriteCurrentTourState>(new WriteCurrentTourState("req1", dict), TimeSpan.FromSeconds(1)); // Expect Answer
-
-        Assert.AreEqual(reply.id, "req1");
-
-    }
-
-}
 }
