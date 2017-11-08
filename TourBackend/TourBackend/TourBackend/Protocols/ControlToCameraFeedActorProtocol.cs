140217fd294a26cf1bdfd4dc7def0bef75051b41
Moritz Scherer <scheremo@student.ethz.ch>
2017-11-08 14:33:35 +0100

    TourBackend ?nderungen
    Implementation des Testings zu CameraFeedActor
    Implementation des Testings zu CameraFeedSyncObject
    
    Implementation CameraFeedSyncObject
    Implementation CameraFeedActor
    
    Bereitstellung von Utilityfunktionen zum Testen des Frameworks mit lokalen Frames

--- TourBackend/TourBackend/TourBackend/Protocols/ControlToCameraFeedActorProtocol.cs
+++ TourBackend/TourBackend/TourBackend/Protocols/ControlToCameraFeedActorProtocol.cs
@@ -3,18 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Proto;
 
 namespace TourBackend
 {
-    public class CameraFeedSyncObject {
+    public class CameraFeedSyncObject
+    {
+
+        public Int64 timestamp;
 
         public Windows.Graphics.Imaging.SoftwareBitmap bitmap;
         public string id;
         public object thisLock = new Object();
 
-        public CameraFeedSyncObject(string _id, Windows.Graphics.Imaging.SoftwareBitmap _bitmap) {
+        public event EventHandler FrameUpdated;
+
+        public CameraFeedSyncObject(string _id)
+        {
+            id = _id;
+        }
+
+        protected void OnFrameUpdated(EventArgs e)
+        {
+            EventHandler handler = FrameUpdated;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+
+        public void UpdateFrame()
+        {
+            OnFrameUpdated(EventArgs.Empty);
+        }
+
+    }
+
+    public class NewFrameArrived
+    {
+        public string id;
+        public Windows.Graphics.Imaging.SoftwareBitmap bitmap;
+
+        public NewFrameArrived(string _id, Windows.Graphics.Imaging.SoftwareBitmap _bitmap)
+        {
             id = _id;
             bitmap = _bitmap;
         }
     }
+
 }
---
+++ TourBackend/TourBackend/TourBackend/Utils/UseCustomVideoFrameSource.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Graphics.Imaging;
+
+namespace TourBackend
+{
+    public static class Utils
+    {
+        // Hilfsfunktion. Wandelt einen "normalen" Stream in einen RandomAccessStream um
+        public static async Task<Windows.Storage.Streams.IRandomAccessStream> ConvertToRandomAccessStream(Stream _stream)
+        {
+            var memoryStream = new MemoryStream();
+            await _stream.CopyToAsync(memoryStream);
+
+            var randomAccessStream = new Windows.Storage.Streams.InMemoryRandomAccessStream();
+
+            var outputStream = randomAccessStream.GetOutputStreamAt(0);
+            var dw = new Windows.Storage.Streams.DataWriter(outputStream);
+            var task = new Task(() => dw.WriteBytes(memoryStream.ToArray()));
+            task.Start();
+
+            await task;
+            await dw.StoreAsync();
+
+            await outputStream.FlushAsync();
+
+            return randomAccessStream;
+        }
+
+        // Stellt einen Testframe zu Verfügung
+        public static async Task<SoftwareBitmap> CreateTestFrame(Stream _stream)
+        {
+
+            string temp = Path.GetTempFileName();
+
+            if (_stream != null)
+            {
+                System.Drawing.Image.FromStream(_stream).Save(temp);
+            }
+
+            var inputFile = await Windows.Storage.StorageFile.GetFileFromPathAsync(temp);
+
+            Windows.Graphics.Imaging.SoftwareBitmap softwareBitmap;
+
+            using (Windows.Storage.Streams.IRandomAccessStream stream = await inputFile.OpenAsync(Windows.Storage.FileAccessMode.Read))
+            {
+                Windows.Graphics.Imaging.BitmapDecoder decoder = await Windows.Graphics.Imaging.BitmapDecoder.CreateAsync(stream);
+                softwareBitmap = await decoder.GetSoftwareBitmapAsync();
+            }
+
+            return softwareBitmap;
+        }
+    }
+}
