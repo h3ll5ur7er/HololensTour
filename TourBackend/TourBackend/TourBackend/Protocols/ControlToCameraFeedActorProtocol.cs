e4086b5cf6f44b4dcab6d37b59574c2b402f4578
Moritz Scherer <scheremo@student.ethz.ch>
2017-11-08 08:19:08 +0100

    TourBackend Änderungen
    Installieren des Kompatibilitätspackages UWPDesktop
    Einfügen der Klasse CameraFeedSyncObject
    Definitionen eingefügt

---
+++ TourBackend/TourBackend/TourBackend/Protocols/ControlToCameraFeedActorProtocol.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TourBackend
+{
+    public class CameraFeedSyncObject {
+        public Windows.Graphics.Imaging.SoftwareBitmap bitmap;
+        public string id;
+        public object thisLock = new Object();
+
+        public CameraFeedSyncObject(string _id, Windows.Graphics.Imaging.SoftwareBitmap _bitmap) {
+            id = _id;
+            bitmap = _bitmap;
+        }
+    }
+}
