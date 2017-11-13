d5c084597c39abc3bb0d52e5ae6e81e7bab5d268
Moritz Scherer <scheremo@student.ethz.ch>
2017-11-13 13:51:37 +0100

    TourBackend Änderungen
    Integrationstest für das GesamtFrameWork angepasst

--- TourBackend/TourBackend/UnitTestProject1/CameraFrameActor-ControlActor_IntegrationTest.cs
+++ TourBackend/TourBackend/UnitTestProject1/CameraFrameActor-ControlActor_IntegrationTest.cs
@@ -19,9 +19,8 @@ namespace TourBackend
         public async Task A_single_frame_needs_to_travel_from_CameraFeedSyncObject_To_SyncActor_with_mock_Frames()
         {
             SoftwareBitmap testframe;
-            CameraFeedSyncObject test = new CameraFeedSyncObject("new");
-            object msg = new Object();
-            var pidsyncactor = new PID();
+            CameraFeedSyncObject camerafeedsyncobject = new CameraFeedSyncObject("new");
+            SyncObject syncobject = new SyncObject("sync1", new Dictionary<string, CodeObject>());
 
             //This dict will have to be updated to be true to the frame
             var dict = new Dictionary<string, CodeObject>();
@@ -31,14 +30,8 @@ namespace TourBackend
             dict.Add("cd1", cd1);
             dict.Add("cd2", cd2);
 
-            var propstest = Actor.FromProducer(() => new TestActor(ref msg));
-            var pidtest = Actor.Spawn(propstest);
-
-            var syncobj = new SyncObject("sync1", new Dictionary<string, CodeObject>());
-            var syncobj2 = new CameraFeedSyncObject("sync2");
-
-            var propsctrl = Actor.FromProducer(() => new ControlActor("ctrl", syncobj, syncobj2, ref pidsyncactor, 2));
-            var pidctrl = Actor.Spawn(propsctrl);
+            FrameWork fw = new FrameWork(syncobject, camerafeedsyncobject);
+            fw.Initialize();
 
             // Creates a testframe from local bitmaps
             var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
@@ -47,25 +40,78 @@ namespace TourBackend
             Stream testfile = File.OpenRead(path);
             testframe = await Utils.CreateTestFrame(testfile);
 
-            lock (test.thisLock)
+            lock (camerafeedsyncobject.thisLock)
             {
-                test.bitmap = testframe;
-                test.timestamp = 110100010;
+                camerafeedsyncobject.bitmap = testframe;
+                camerafeedsyncobject.timestamp = 110100010;
             }
             // The timestamp is also the message id
 
-            test.UpdateFrame();
+            camerafeedsyncobject.UpdateFrame();
 
             // See if the output has been updated within 1 second
             Stopwatch stop = new Stopwatch();
             stop.Start();
-            while (stop.ElapsedMilliseconds < 1000 && syncobj.dict != dict) {
+            while (stop.ElapsedMilliseconds < 1000 && syncobject.dict != dict) {
                 Thread.Sleep(5); // Arbitrary sleep length
             }
             stop.Stop();
 
             // Fail test if syncobj hasn't been updated
-            CollectionAssert.AreEqual(syncobj.dict, dict);
+            CollectionAssert.AreEqual(syncobject.dict, dict);
         }
+
+        [TestMethod]
+        public async Task Multiple_frames_need_to_travel_from_CameraFeedSyncObject_To_SyncActor_with_mock_Frames()
+        {
+            SoftwareBitmap testframe;
+            CameraFeedSyncObject camerafeedsyncobject = new CameraFeedSyncObject("new");
+            SyncObject syncobject = new SyncObject("sync1", new Dictionary<string, CodeObject>());
+
+            //This dict will have to be updated to be true to the frame
+            var dict = new Dictionary<string, CodeObject>();
+            var cd1 = new CodeObject("cd1", 1, new[] { 1f, 2f, 3f }, new[] { 4f, 5f, 6f });
+            var cd2 = new CodeObject("cd2", 1, new[] { 4f, 7f, 8f }, new[] { 1f, 19f, 3f }); // Just build two "random" CodeObjects
+
+            dict.Add("cd1", cd1);
+            dict.Add("cd2", cd2);
+
+            FrameWork fw = new FrameWork(syncobject, camerafeedsyncobject);
+            fw.Initialize();
+
+            for (int i=0;i<5;i++) {
+
+                // Creates a testframe from local bitmaps
+                var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                path = Path.Combine(path, "Resources");
+                path = Path.Combine(path, "TestVideo_000.bmp");
+                Stream testfile = File.OpenRead(path);
+                testframe = await Utils.CreateTestFrame(testfile);
+
+                lock (camerafeedsyncobject.thisLock)
+                {
+                    camerafeedsyncobject.bitmap = testframe;
+                    camerafeedsyncobject.timestamp = 110100010;
+                }
+                // The timestamp is also the message id
+
+                camerafeedsyncobject.UpdateFrame();
+
+
+
+                // See if the output has been updated within 1 second
+                Stopwatch stop = new Stopwatch();
+                stop.Start();
+                while (stop.ElapsedMilliseconds < 1000 && syncobject.dict != dict)
+                {
+                    Thread.Sleep(5); // Arbitrary sleep length
+                }
+                stop.Stop();
+
+                // Fail test if syncobj hasn't been updated
+                CollectionAssert.AreEqual(syncobject.dict, dict);
+            }
+        }
+
     }
 }
