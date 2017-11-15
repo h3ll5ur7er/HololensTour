aecd66e484da3c89bf5efd133f01283ad9453a85
Moritz Scherer <scheremo@student.ethz.ch>
2017-11-15 14:35:22 +0100

    TourBackend Änderungen
    Klasse hinzugefügt, die eine tiefe Kopie vom Dictionary erlaubt, das die CodeObjects beinhaltet

---
+++ TourBackend/TourBackend/TourBackend/Utils/CopySyncDict.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TourBackend.Utils
+{
+    public static class CopySyncDict
+    {
+        public static Dictionary<string, CodeObject> Copy(Dictionary<string, CodeObject> _dict) {
+            Dictionary<string, CodeObject> copy = new Dictionary<string, CodeObject>();
+
+            foreach(KeyValuePair<string, CodeObject> p in _dict){
+                copy.Add(p.Key, new CodeObject(p.Value));
+            }
+
+            return copy;
+        }
+    }
+}
