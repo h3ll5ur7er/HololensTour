63ed7384050fd03a5464140c510bbda2a417b38e
Moritz Scherer <scheremo@student.ethz.ch>
2017-11-27 15:02:53 +0100

    TourBackend Änderungen
    Projekt in .NET Standard 2.0 portiert

---
+++ TourBackend/TourBackend/TourBackend .NETStandard/Actors/ControlActor.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Proto;
+using System.Threading;
+
+namespace TourBackend
+{
+
+    public class FrameWork {
+        public CodeObject[] markers;
+
+        public Dictionary<int, CodeObject> idToCodeObject;
+
+        private PID pidctrl;
+        private SyncObject syncobj;
+        private CameraFeedSyncObject video;
+
+
+        public FrameWork(SyncObject _syncobj, CameraFeedSyncObject _video, CodeObject[] _markers) {
+            syncobj = _syncobj;
+            video = _video;
+            markers = _markers;
+        }
+
+        public void Initialize()
+        {
+            var propsctrl = Actor.FromProducer(() => new ControlActor("ctrl", syncobj, video, idToCodeObject));
+            pidctrl = Actor.Spawn(propsctrl);
+
+            pidctrl.RequestAsync<RespondStartFramework>(new StartFramework(pidctrl), TimeSpan.FromSeconds(1));
+        }
+
+        public PID GetPID() {
+            return this.pidctrl;
+        }
+
+    }
+
+    public class StartFramework {
+        public PID ctrl;
+        public StartFramework(PID _ctrl) {
+            ctrl = _ctrl;
+        }
+    }
+
+    public class RespondStartFramework {
+        public RespondStartFramework(){ }
+    }
+
+    // Attention: This Actor is subject to change!
+    // Neither the constructors nor any other function is guaranteed to stay unchanged!
+
+    public class ControlActor : IActor
+    {
+        public string id { get; }
+        public SyncObject sync;
+        public CameraFeedSyncObject video; // This field will *certainly* change
+        public PID recognitionManager;
+        public PID syncActor;
+        public PID cameraFeedSyncActor;
+        public PID self;
+        public Dictionary<int, CodeObject> dict;
+        // More managed PIDs are to follow
+
+        public void Start() {
+            var syncprops = Actor.FromProducer(() => new SyncActor("syncactor", sync));
+            this.syncActor = Actor.Spawn(syncprops);
+
+            var recogprops = Actor.FromProducer(() => new RecognitionManager("recognitionmanager", dict));
+            this.recognitionManager = Actor.Spawn(recogprops);
+
+            var cameraFeedSyncprops = Actor.FromProducer(() => new CameraFeedActor("camerafeedactor", video, self));
+            this.recognitionManager = Actor.Spawn(cameraFeedSyncprops);
+        }
+
+        public ControlActor(string _id, SyncObject _sync, CameraFeedSyncObject _video, Dictionary<int, CodeObject> _dict)
+        {
+            id = _id;
+            sync = _sync;
+            video = _video;
+            dict = _dict;
+        }
+
+        // This constructor is only meant for debugging 
+        // It is functionally identically to the usual constructor except that it 
+        // links the reference _debugPID to either of the newly created Actors, chosen according to the int argument,
+        // as this allows for more convenient testing.
+        // It also doesn't spawn the CameraFeedActor
+        // Key for the int argument:
+        // 1: RecognitionManager
+        // 2: SyncActor
+
+        public ControlActor(string _id, SyncObject _sync, CameraFeedSyncObject _video, ref PID _debugPID, int debug)
+        {
+            id = _id;
+            sync = _sync;
+            video = _video;
+
+            var syncprops = Actor.FromProducer(() => new SyncActor("syncactor", sync));
+            this.syncActor = Actor.Spawn(syncprops);
+
+            var recogprops = Actor.FromProducer(() => new RecognitionManager("recognitionmanager", dict));
+            this.recognitionManager = Actor.Spawn(recogprops);
+
+            switch (debug)
+            {
+                case 1:
+                    _debugPID = recognitionManager;
+                    break;
+                case 2:
+                    _debugPID = syncActor;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+
+
+        public Task ReceiveAsync(IContext context)
+        {
+            var msg = context.Message;
+            switch (msg)
+            {
+                case StartFramework s:
+                    self = s.ctrl;
+                    this.Start();
+                    context.Sender.Tell(new RespondStartFramework());
+                    break;
+                case NewFrameArrived n:
+                    recognitionManager.Tell(n);
+                    break;
+                case RespondNewFrameArrived r:
+                    recognitionManager.Request(new RequestAllVirtualObjects(r.messageID,TimeSpan.FromMilliseconds(25)), self);
+                break;
+                case RespondRequestAllVirtualObjects r:
+                    syncActor.Request(new WriteCurrentTourState(r.messageID, r.newCodeObjectIDToCodeObject), self);
+                    break;
+            }
+            return Actor.Done;
+        }
+
+    }
+}
---
+++ TourBackend/TourBackend/TourBackend .NETStandard/Actors/RecognitionManager.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Proto;
+using OpenCvSharp;
+//using OpenCvSharp.Extensions;
+using System.Drawing;
+using OpenCvSharp.Aruco;
+
+namespace TourBackend
+{
+    public class RecognitionManager : IActor
+    {
+        // this is the id of this actor
+        protected string Id { get; }
+        // this dictionary is created when the recognition manager gets initialized by the controlactor and
+        // it stays static meaning that the keys to value relations will not change. The only thing that changes
+        // are the codeObject itself id est their properties.
+        public Dictionary<int,CodeObject> codeObjectIDToCodeObject = new Dictionary<int, CodeObject>();
+        // here is the constructor
+        public RecognitionManager(string id, Dictionary<int, CodeObject> _dict)
+        {
+            Id = id;
+            codeObjectIDToCodeObject = _dict;
+        }
+        // here we write all actions if we receive a certain message
+        public Task ReceiveAsync(IContext context)
+        {
+            var msg = context.Message;
+            switch (msg)
+            {
+                // the idea here is that if we get the requestall virtual objects we check what we have in 
+                // the dictionary codeObjectIDToCodeObject and then we create a new Dictionary with only the 
+                // CodeObjects in it which have the isActive == true
+                case RequestAllVirtualObjects r:
+                    {
+                        // first create a new empty dictionary
+                        Dictionary<int, CodeObject> returnDict = new Dictionary<int, CodeObject>();
+                        // for each codeObject in the dictionary, add the key value pair into the returnDict
+                        // if and only if his isActive == true
+                        foreach (var entry in codeObjectIDToCodeObject)
+                        {
+                            if(entry.Value.isActive == true)
+                            {
+                                returnDict.Add(entry.Key, entry.Value);
+                            }
+                        }
+                        // if we now have the return Dictionary, send the Respond message back to the sender
+                        var _respondToRequestMessage = new RespondRequestAllVirtualObjects(r.messageID, returnDict);
+                        context.Sender.Tell(_respondToRequestMessage);
+                    }
+                    break;
+
+                // the idea here is if we get the setActive message we should look up in the dictionary if the 
+                // requested CodeObject does exitst. If it exists we should set the isActive to true. And then 
+                // send back the response to the sender
+                case SetActiveVirtualObject s:
+                    {
+                        if (codeObjectIDToCodeObject.ContainsKey(s.toBeActiveVirtualObjectID))
+                        {
+                            codeObjectIDToCodeObject[s.toBeActiveVirtualObjectID].isActive = true;
+                            // respond to the sender
+                            var _respondMsg = new RespondSetActiveVirtualObject(s.messageID, s.toBeActiveVirtualObjectID);
+                            context.Sender.Tell(_respondMsg);
+                        }
+                        else
+                        {
+                            // respond to the sender with a failedToMessage
+                            var _failMsg = new FailedToSetActiveVirtualObject(s.messageID);
+                            context.Sender.Tell(_failMsg);
+                        }
+                    }
+                    break;
+
+                // the idea here is if we get the setInActive message we should look up in the dictionary if the 
+                // requested CodeObject does exitst. If it exists we should set the isActive to false. And then 
+                // send back the response back to the sender.
+                case SetInActiveVirtualObject s:
+                    {
+                        if (codeObjectIDToCodeObject.ContainsKey(s.toBeInActiveVirtualObjectID))
+                        {
+                            codeObjectIDToCodeObject[s.toBeInActiveVirtualObjectID].isActive = false;
+                            // respond to the sender
+                            var _respondMsg = new RespondSetInActiveVirtualObject(s.messageID, s.toBeInActiveVirtualObjectID);
+                            context.Sender.Tell(_respondMsg);
+                        }
+                        else
+                        {
+                            // respond to the sender with a failedToMessage
+                            var _failMsg = new FailedToSetInActiveVirtualObject(s.messageID);
+                            context.Sender.Tell(_failMsg);
+                        }
+                    }
+                    break;
+
+                case NewFrameArrived n:
+                    {
+                        // do the work here with the recognition of the frame
+                        FrameEvaluation(n.bitmap);
+                        // after the successfull evaluation respond to the control Actor
+                        var _respondMsg = new RespondNewFrameArrived(n.id);
+                        context.Sender.Tell(_respondMsg);
+                    }
+                    break;
+            }
+            return Actor.Done;
+        }
+
+        /// <summary>
+        /// the idea here is that we use this function to recognize the markers in the bitmap and update then all
+        /// markers in the dictionary with their position and rotation etc. 
+        /// </summary>
+        public void FrameEvaluation(Windows.Graphics.Imaging.SoftwareBitmap _bitmap)
+        {
+            /* use the Function from OpenCVSharp.Aruco.CvAruco for the detection: 
+             * public static void DetectMarkers(InputArray image, Dictionary dictionary, out Point2f[][] corners, out int[] ids, DetectorParameters parameters, out Point2f[][] rejectedImgPoints)
+             * 
+             */
+
+            // Mat _mat = BitmapConverter.ToMat(_bitmap);
+            // indicates the type of markers that will be searched
+            OpenCvSharp.Aruco.Dictionary _dict = OpenCvSharp.Aruco.CvAruco.GetPredefinedDictionary(PredefinedDictionaryName.DictArucoOriginal);
+            // _dict=0; vector of detected marker corners.
+            // For each marker, its four corners are provided. For N detected markers,
+            // the dimensions of this array is Nx4.The order of the corners is clockwise.
+
+            // vector of identifiers of the detected markers. The identifier is of type int.
+            // For N detected markers, the size of ids is also N. The identifiers have the same order than the markers 
+            // in the imgPoints array.
+
+            // marker detection parameters
+            OpenCvSharp.Aruco.DetectorParameters _parameters = DetectorParameters.Create();
+
+            // contains the imgPoints of those squares whose inner code has not a 
+            // correct codification. Useful for debugging purposes.
+
+            //OpenCvSharp.Aruco.CvAruco.DetectMarkers(_mat, _dict, out Point2f[][] _corners, out int[] _ids, _parameters , out Point2f[][] _rejectedImgPoints);
+
+        }
+
+    }
+}
